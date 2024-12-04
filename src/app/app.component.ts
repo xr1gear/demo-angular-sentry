27104c6aca3d8d1cf202c6789ddcf078df3649ba
Eugene Beliaikine <ebeliaikine@routeone.com>
2024-12-04 13:44:17 -0500

    Add: timeout for error button

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -8,12 +8,23 @@ import {SharedStateService} from "./shared-state.service";
 })
 export class AppComponent {
   title = 'demo-angular-sentry';
+  isTimeoutReached: boolean = false;
 
-  constructor(public sharedStateService: SharedStateService) { }
+  constructor(public sharedStateService: SharedStateService) {
+
+  }
 
   throwError() {
     throw Error("Sample error: " + this.sharedStateService.userName + " id: " + this.sharedStateService.randomId);
   }
 
+  isEnabledAfterUsernameAnd60SecondTimeout() {
+    if(this.sharedStateService.userName) {
+      setTimeout(() => {
+        this.isTimeoutReached = true;
+      }, 60000);
+    }
 
+    return this.sharedStateService.userName && this.isTimeoutReached;
+  }
 }
